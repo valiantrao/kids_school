7f2038fc0f178e96d8abfed63000997540c07368
valiantrao <valiantrao@users.noreply.github.com>
2020-11-10 19:41:03 +0530

    Initial commit

---
+++ app/src/main/java/com/valiant/kidsschool/MainActivity.java
@@ -0,0 +1,24 @@
+package com.valiant.kidsschool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //full screen---
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
